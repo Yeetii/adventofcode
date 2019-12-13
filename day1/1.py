6e0f7d2c0343602eb11593ffb9e4a23e01d753ea
Erik Magnusson <magnusserik@gmail.com>
2019-12-13 01:20:54 +0100

    Finished 1.2

--- day1/1.py
+++ day1/1.py
@@ -1,11 +1,21 @@
-def calc_on_string(value):
-    value = int(value)
+def calc_first(value):
     value /= 3
     return value - 2
 
+def calc_snd(value):
+    val2 = calc_first(value)
+    if val2 < 0:
+        return 0
+    else:
+        return calc_snd(val2) + val2
+
 f = open('input.txt', 'r')
 x = f.read().splitlines()
-x = map(calc_on_string, x)
-print(sum(x))
 f.close()
+x = map(int, x)
 
+first = map(calc_first, x)
+print(sum(first))
+
+second = map(calc_snd, x)
+print(sum(second))
