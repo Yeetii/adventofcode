a14560c636467214ceda6d645db3a75744935350
Erik Magnusson <contact@erikmagnusson.com>
2022-12-01 10:53:32 +0100

    Skeleton for day2

---
+++ 2022/day2/day2.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = System.IO.File.ReadAllLines(@"testinput.txt");
+            
+        }
+    }
+}
