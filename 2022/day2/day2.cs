5cc37c7ff3980e8a63638c906453f6c2dfa58b07
Erik Magnusson <contact@erikmagnusson.com>
2022-12-02 13:20:32 +0100

    Solution for part 2

--- 2022/day2/day2.cs
+++ 2022/day2/day2.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace AdventOfCode2022
 {
@@ -9,6 +7,12 @@ namespace AdventOfCode2022
         static void Main(string[] args)
         {
             string[] input = System.IO.File.ReadAllLines(@"input.txt");
+            
+            Console.WriteLine(CalculatePartOneScore(input));
+            Console.WriteLine(CalculatePartTwoScore(input));
+        }
+
+        static int CalculatePartOneScore(string[] input){
             int totalScore = 0;
             foreach(string line in input){
                 string[] splits = line.Split(' ');
@@ -16,7 +20,18 @@ namespace AdventOfCode2022
                 Hand player = DecodeStringHand(splits[1]);
                 totalScore += CalculateRoundScore(opponent, player);
             }
-            Console.WriteLine(totalScore);
+            return totalScore;
+        }
+
+        static int CalculatePartTwoScore(string[] input){
+            int totalScore = 0;
+            foreach(string line in input){
+                string[] splits = line.Split(' ');
+                Hand opponent = DecodeStringHand(splits[0]);
+                Hand player = DecideHandFromStrategy(splits[1], opponent);
+                totalScore += CalculateRoundScore(opponent, player);
+            }
+            return totalScore;
         }
 
         static Hand DecodeStringHand(string hand){
@@ -30,6 +45,30 @@ namespace AdventOfCode2022
                 throw new Exception("Hand could not be decoded");
             }
 
+        }
+
+        static Hand DecideHandFromStrategy(string strategy, Hand opponent){
+            if (strategy == "X"){
+                if (opponent == Hand.Rock){
+                    return Hand.Scissor;
+                } else if (opponent == Hand.Paper){
+                    return Hand.Rock;
+                } else {
+                    return Hand.Paper;
+                }
+            } else if (strategy == "Y"){
+                return opponent;
+            } else if (strategy == "Z"){
+                if (opponent == Hand.Rock){
+                    return Hand.Paper;
+                } else if (opponent == Hand.Paper){
+                    return Hand.Scissor;
+                } else {
+                    return Hand.Rock;
+                }
+            } else {
+                throw new Exception("Strategy could not be decoded");
+            }
         }
 
         enum Hand {
